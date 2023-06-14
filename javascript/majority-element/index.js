e7f6b2070f52cfa830d635e716f4ed98b5d542b2
Jason Amey <jason.amey@gmail.com>
2023-06-14 17:25:04 -0400

    add majority element

---
+++ javascript/majority-element/index.js
@@ -0,0 +1,18 @@
+// [1, 2, 3, 2, 2, 1, 2]
+
+function majorityElement (arr) {
+  let foundNum = null
+  let count = 0
+  for (let num of arr) {
+    if (count === 0) foundNum = num
+    if (foundNum === num) {
+      count++
+    } else {
+      count--
+    }
+  }
+  return foundNum
+}
+
+console.log(majorityElement([1, 2, 3, 1, 1]))
+
--- javascript/sum-of-linked-lists/index.js
+++ javascript/sum-of-linked-lists/index.js
@@ -14,54 +14,65 @@ class LinkedList {
 
   }
 }
-
 function sumOfLinkedLists (linkedListOne, linkedListTwo) {
-  let l1 = linkedListOne
-  let l2 = linkedListTwo
-  let total = 0, carry = 0, digit = 0, digitTotal = 0, exp = 0;
-  while (l1 !== null && l2 !== null) {
-    console.log(l1.value)
-    digitTotal = l1.value + l2.value + carry
-    digit = digitTotal % 10
-    total += digit * Math.pow(10, exp++)
-    carry = digit > 9 ? 1 : 0
+  l1 = linkedListOne
+  l2 = linkedListTwo
+  total = 0
+  digit = 0
+  tempTotal = 0
+  exp = 0
+  carry = 0
+  while (l1 !== undefined && l2 !== undefined) {
+    console.log(l1.value, l2.value, tempTotal, total)
+    tempTotal = l1.value + l2.value + carry
+    digit = tempTotal % 10
+    if (tempTotal > 9) {
+      carry = 1
+    } else {
+      carry = 0
+    }
+    total += Math.pow(10, exp++) * digit
+    console.log("total ", total)
     l1 = l1.next
     l2 = l2.next
   }
 
-  if (l1 !== null) {
-    while (l1 !== null) {
-      digitTotal = l1.value + carry
-      digit = digitTotal % 10
-      total += digit * Math.pow(10, exp++)
-      carry = digit > 9 ? 1 : 0
-      l1 = l1.next
+  node = l1 === undefined ? l2 : l2 === undefined ? undefined : l2
+  while (node !== undefined) {
+    tempTotal = node.value + carry
+    digit = tempTotal % 10
+    if (tempTotal > 9) {
+      carry = 1
+    } else {
+      carry = 0
     }
-  } else {
-    while (l2 !== null) {
-      digitTotal = l2.value + carry
-      digit = digitTotal % 10
-      total += digit * Math.pow(10, exp++)
-      carry = digit > 9 ? 1 : 0
-      l2 = l2.next
-    }
+    total += Math.pow(10, exp++) * digit
+    node = node.next
   }
-  if (carry === 1) {
-    total += Math.pow(10, exp)
+  digits = []
+  while (total != 0) {
+    digits.push(new LinkedList(total % 10, null))
+    total = Math.floor(total / 10)
   }
-  console.log(total)
-  return linkedListOne;
+  for (let i = 0; i < digits.length - 1; i++) {
+    digits[i].next = digits[i + 1]
+  }
+  return digits[0];
 }
 
 
-SIZE = 10
-// const list1 = new LinkedList(1, null)
+SIZE = 4
+
 let nodes1 = []
 let nodes2 = []
 for (let i = 1; i < SIZE; i++) {
-  nodes1.push(new LinkedList(Math.floor(1 + Math.random() * 9), null))
+  // nodes1.push(new LinkedList(Math.floor(Math.random() * 9), null))
+  nodes1.push(new LinkedList(i, null))
+
   nodes2.push(new LinkedList(i, null))
 }
+
+
 
 for (let i = 0; i < SIZE - 1; i++) {
   nodes1[i].next = nodes1[i + 1]
@@ -69,7 +80,7 @@ for (let i = 0; i < SIZE - 1; i++) {
 }
 
 nodes1[0].printList()
-
+nodes2[0].printList()
 // nodes2[0].printList()
 // console.log(nodes1[0])
 sumOfLinkedLists(nodes1[0], nodes2[0])
