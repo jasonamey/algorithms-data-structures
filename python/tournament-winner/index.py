b8cd788441eda12072143de0589482ee9d5db934
Jason Amey <jason.amey@gmail.com>
2023-06-15 22:05:27 -0400

    add-tournaments and find closest-value and branch-sums

--- python/tournament-winner/index.py
+++ python/tournament-winner/index.py
@@ -1,21 +1,20 @@
 from collections import defaultdict
 
-
-
 def tournamentWinner(competitions, results):
-      dd = defaultdict(int)
-      max = 0
-      winningTeam = ''
-      for i in range(len(results)): 
-          winningTeamIdx = results[i]
-          winningTeamIdx = 1 if winningTeamIdx == 0 else 0
-          teamName = competitions[i][winningTeamIdx]
-          dd[teamName] += 1
-      for winner in dd: 
-          if dd[winner] > max: 
-              max = dd[winner]
-              winningTeam = winner
-      return winningTeam
+    dd = defaultdict(int)
+    for i in range(0, len(results)):
+        winnerIdx = results[i]
+        i = 0 if i == 1 else 1
+        winner = competitions[i][winnerIdx]
+        dd[winner] = dd[winner] + 1
+        print(dd)
+    maxWins = 0
+    currWinner = ""
+    for team in dd:
+        if dd[team] > maxWins:
+            maxWins = dd[team]
+            currWinner = team
+    return currWinner
 
 # c = [
 #     ["HTML", "C#"],
