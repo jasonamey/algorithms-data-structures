7a62adbf650e97d4bbc179b34bb555d9ade5f8c4
Jason Amey <jason.amey@gmail.com>
2023-06-18 00:55:50 -0400

    add class-photos remove-linked-list-duplicates min-wait-times tandem-bike

--- python/minimum-wait-times/index.py
+++ python/minimum-wait-times/index.py
@@ -1,9 +1,7 @@
 def minimumWaitingTime(queries):
     queries.sort()
-    print(queries)
     total = 0
     for i in range(0, len(queries) - 1): 
-        print(i, " ", len(queries) - 1 - i, " ", queries[i] * len(queries) - 1 - i) 
         total += queries[i] * (len(queries) - 1 - i )
     return total
 
