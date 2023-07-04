071b36b992767cbdc91265e1b88470d952190880
Jason Amey <jason.amey@gmail.com>
2023-07-04 15:35:38 -0400

    add balanced-brackets and min-max-stack

---
+++ interview-prep-site/python/balanced-brackets/index.py
@@ -0,0 +1,17 @@
+def balancedBrackets(string):
+    opened = "([{"
+    closed = ")]}"
+    stack = []
+    for bracket in string:
+        if bracket in opened:
+            stack.append(bracket)
+        elif bracket in closed:
+            if len(stack) == 0:
+                return False
+            test = stack.pop()
+            if opened.index(test) != closed.index(bracket):
+                return False
+    return len(stack) == 0
+
+
+print(balancedBrackets("(([()()){})"))
