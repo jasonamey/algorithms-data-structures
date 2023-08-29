081acbc027c2e33a9ad76b47af3e8476808ddaa9
Jason Amey <jason.amey@gmail.com>
2023-08-29 19:57:33 -0400

    208 Trie in Leetcode

---
+++ leetcode/python/208-trie.py
@@ -0,0 +1,34 @@
+class TrieNode:
+    def __init__(self): 
+        self.end = False 
+        self.children = {}
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        cur = self.root 
+        for ch in word: 
+            if ch not in cur.children:
+                cur.children[ch] = TrieNode()
+            cur = cur.children[ch]
+        cur.end = True
+        
+    def search(self, word: str) -> bool:
+        cur = self.root
+        for ch in word: 
+            if ch not in cur.children:
+                return False
+            cur = cur.children[ch]
+        return cur.end == True 
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        cur = self.root 
+        for ch in prefix: 
+            if ch not in cur.children:
+                return False
+            cur = cur.children[ch]
+        return True
+        
