9242dfed0b884bbb21b5cc8b0315469f09ce6f4c
Jason Amey <jason.amey@gmail.com>
2023-11-14 22:05:16 -0500

    add Logan binary search

--- leetcode/python/704-binary-search.py
+++ leetcode/python/704-binary-search.py
@@ -11,6 +11,32 @@ def search(nums: list[int], target: int) -> int:
             lo = mid + 1
     return -1
 
+#
+# class Solution_logan:
+#     def search(self, nums: List[int], target: int) -> int:
+
+#         start = 0
+#         end = len(nums) - 1
+
+#         while start <= end:
+#             mid = (start + end) // 2
+
+#             if nums[mid] == target:
+#                 return mid
+
+#             if nums[mid] >= nums[start]:
+#                 if nums[start] <= target <= nums[mid]:
+#                     end = mid - 1
+#                 else:
+#                     start = mid + 1
+#             else:
+#                 if nums[mid] <= target <= nums[end]:
+#                     start = mid + 1
+#                 else:
+#                     end = mid - 1
+
+#         return -1
+
 
 assert search([-1,0,3,5,9,12], 9) == 4
 assert search([-1,0,3,5,9,12], 2) == -1
