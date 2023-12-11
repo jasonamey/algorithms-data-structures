d273099ee72b66e2d6dd10a134b95ea34101faff
Jason Amey <jason.amey@gmail.com>
2023-12-11 15:29:37 -0500

    change solution for LC 74

--- leetcode/python/medium/74-search-2D-matrix.py
+++ leetcode/python/medium/74-search-2D-matrix.py
@@ -11,31 +11,18 @@
 # You must write a solution in O(log(m * n)) time complexity.
 
 class Solution:
-    def searchMatrix(self, matrix, target):
-        y = 0 
-        x = len(matrix[0]) - 1
-        test = matrix[y][x]
-        if test == target: 
-            return True
-        while (x > 0 and y < len(matrix)): 
-            if test > target: 
-                x = x - 1
-                if x >= 0: 
-                    test = matrix[y][x]
-                    if test == target: 
-                        return True 
-                else: 
-                    return False 
-            else:
-                y = y + 1
-                if y < len(matrix): 
-                    test = matrix[y][x]
-                    if test == target: 
-                        return True
-                else: 
-                    return False  
-        return False 
-        
+    def searchMatrix(self, m, target):
+        x = len(m[0]) - 1 
+        y = 0
+        while (x >= 0 and y < len(m)): 
+            num = m[y][x]
+            if num == target: 
+                return True
+            elif num < target: 
+                y += 1
+            else: 
+                x -= 1 
+        return False
 s = Solution()
 
 matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]] 
