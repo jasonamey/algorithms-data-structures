c4f21790ee79391f259debacfc73f29f7cd57bb8
Jason Amey <jason.amey@gmail.com>
2024-02-22 19:21:07 -0500

    add LC 622 Circular Queue

---
+++ leetcode/python/medium/622-design-circular-queue.py
@@ -0,0 +1,46 @@
+class MyCircularQueue:
+    def __init__(self, k: int):
+        self.q = [None for _ in range(k)]
+        self.front = 0
+        self.size = 0
+
+    def enQueue(self, value: int) -> bool:
+        if self.isFull():
+            return False
+        else: 
+            self.q[self.getBack()] = value
+            self.size += 1
+            return True
+
+    def deQueue(self) -> bool:
+        if self.isEmpty():
+            return False
+        else: 
+            self.q[self.front] = None
+            self.front = (self.front + 1) % len(self.q)
+            self.size -= 1
+            return True
+
+    def Front(self) -> int:
+        if self.isEmpty():
+            return -1
+        else: 
+            return self.q[self.front]
+        
+    def Rear(self) -> int:
+        if self.isEmpty():
+            return -1
+        else: 
+            return self.q[(self.front + self.size - 1) % len(self.q)]
+        
+    def isEmpty(self) -> bool:
+        return self.size == 0
+        
+    def isFull(self) -> bool:
+        return self.size == len(self.q)
+        
+    def getBack(self): 
+        return (self.front + self.size) % len(self.q)
+
+    def __str__(self):
+        return f"{self.q}"
