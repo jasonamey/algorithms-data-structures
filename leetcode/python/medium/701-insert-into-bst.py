7072e5e6aa36f20178c0e8e6d56ffdeb63924aed
Jason Amey <jason.amey@gmail.com>
2024-02-27 19:11:29 -0500

    tidying

--- leetcode/python/medium/701-insert-into-bst.py
+++ leetcode/python/medium/701-insert-into-bst.py
@@ -1,11 +1,11 @@
 # Definition for a binary tree node.
 
-# You are given the root node of a binary search tree (BST) and a value to insert into the tree. 
-# Return the root node of the BST after the insertion. It is guaranteed that the new value does 
-# not exist in the original BST.
+# You are given the root node of a binary search tree (BST) and a value to insert 
+# into the tree. Return the root node of the BST after the insertion. It is 
+# guaranteed that the new value does not exist in the original BST.
 
-# Notice that there may exist multiple valid ways for the insertion, as long as the tree 
-# remains a BST after insertion. You can return any of them.
+# Notice that there may exist multiple valid ways for the insertion, 
+# as long as the tree remains a BST after insertion. You can return any of them.
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
